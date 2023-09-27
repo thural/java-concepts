df28bd551f5b8575968381d55091c786a39c5fd1
thural <tural.musaibov@gmail.com>
2023-09-27 20:43:30 +0400

    use internal Predicate functional interface

--- src/lambda_expression/Lambda.java
+++ src/lambda_expression/Lambda.java
@@ -1,15 +1,30 @@
 package lambda_expression;
 
+import java.util.function.Predicate;
+
 public class Lambda {
-    static String printThing(Printable thing){
-       return thing.print("!");
+    static String printThing(Printable thing) {
+        return thing.print("!");
     }
+
     public static void main(String[] args) {
-        // object is redundant since lambda expression creates it
+        // using object of a Class that implements the interface
         Cat myCat = new Cat();
+        printThing(myCat);
+
+        // using an anonymous class that implements the interface
+        Printable printableObject = new Printable() {
+            @Override
+            public String print(String suffix) {
+                return "printing..." + suffix;
+            }
+        };
+
+        String text0 = printThing(printableObject);
+        System.out.println(text0);
 
         // the lambda expression automatically implements an anonymous
-        // class, implements the method as expressed, then provides
+        // class, overrides the method as expressed, then provides
         // an instance of that class where its expressed
         String text1 = printThing((suffix) -> "meow" + suffix);
         System.out.println(text1);
@@ -23,5 +38,11 @@ public class Lambda {
         // storing and printing the return value
         String text3 = lambdaPrintable.print("!!!");
         System.out.println(text3);
+
+        // using internal Predicate functional interface
+        Predicate checkForNull = (o) -> o != null;
+        Object object = null;
+        boolean testResult = checkForNull.test(object);
+        System.out.println(testResult);
     }
 }
